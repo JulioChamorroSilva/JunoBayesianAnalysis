42726ec67ea6c7bd92e7ce89a0efae489f3e0dc4
Pietro Chimenti <pietro.chimenti@gmail.com>
2021-03-08 14:38:39 -0300

    Quickstart from emcee page

--- Tests/BasicTools/test_emcee.py
+++ Tests/BasicTools/test_emcee.py
@@ -1,1 +1,51 @@
 print("Testing the emcee package!")
+
+import numpy as np
+
+def log_prob(x, mu, cov):
+    diff = x - mu
+    return -0.5 * np.dot(diff, np.linalg.solve(cov, diff))
+
+ndim = 5
+
+np.random.seed(42)
+means = np.random.rand(ndim)
+
+cov = 0.5 - np.random.rand(ndim ** 2).reshape((ndim, ndim))
+cov = np.triu(cov)
+cov += cov.T - np.diag(cov.diagonal())
+cov = np.dot(cov, cov)
+
+nwalkers = 32
+p0 = np.random.rand(nwalkers, ndim)
+
+import emcee
+
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_prob, args=[means, cov])
+
+log_prob(p0[0], means, cov)
+
+state = sampler.run_mcmc(p0, 100)
+sampler.reset()
+
+sampler.run_mcmc(state, 10000);
+
+import matplotlib.pyplot as plt
+
+samples = sampler.get_chain(flat=True)
+plt.hist(samples[:, 0], 100, color="k", histtype="step")
+plt.xlabel(r"$\theta_1$")
+plt.ylabel(r"$p(\theta_1)$")
+plt.gca().set_yticks([]);
+plt.show()
+
+print("Mean acceptance fraction: {0:.3f}".format(np.mean(sampler.acceptance_fraction)))
+
+print(
+    "Mean autocorrelation time: {0:.3f} steps".format(
+        np.mean(sampler.get_autocorr_time())
+    )
+)
+
+
+
