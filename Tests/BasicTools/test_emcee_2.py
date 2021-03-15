b28f53d44a04b224966a710c23b1f844bf37ee40
Pietro Chimenti <pietro.chimenti@gmail.com>
2021-03-15 14:32:51 -0300

    testing a linear fit with different methods

---
+++ Tests/BasicTools/test_emcee_2.py
@@ -0,0 +1,102 @@
+# test copied from https://emcee.readthedocs.io/en/stable/tutorials/line/
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(123)
+
+# Choose the "true" parameters.
+m_true = -0.9594
+b_true = 4.294
+f_true = 0.534
+
+# Generate some synthetic data from the model.
+N = 50
+x = np.sort(10 * np.random.rand(N))
+yerr = 0.1 + 0.5 * np.random.rand(N)
+y = m_true * x + b_true
+y += np.abs(f_true * y) * np.random.randn(N)
+y += yerr * np.random.randn(N)
+
+
+# now least square fit
+
+A = np.vander(x, 2)
+C = np.diag(yerr * yerr)
+ATA = np.dot(A.T, A / (yerr ** 2)[:, None])
+cov = np.linalg.inv(ATA)
+w = np.linalg.solve(ATA, np.dot(A.T, y / yerr ** 2))
+print("Least-squares estimates:")
+print("m = {0:.3f} ± {1:.3f}".format(w[0], np.sqrt(cov[0, 0])))
+print("b = {0:.3f} ± {1:.3f}".format(w[1], np.sqrt(cov[1, 1])))
+
+# now maximum likelihood estimation
+
+def log_likelihood(theta, x, y, yerr):
+    m, b, log_f = theta
+    model = m * x + b
+    sigma2 = yerr ** 2 + model ** 2 * np.exp(2 * log_f)
+    return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
+
+from scipy.optimize import minimize
+
+np.random.seed(42)
+nll = lambda *args: -log_likelihood(*args)
+initial = np.array([m_true, b_true, np.log(f_true)]) + 0.1 * np.random.randn(3)
+soln = minimize(nll, initial, args=(x, y, yerr))
+m_ml, b_ml, log_f_ml = soln.x
+
+print("Maximum likelihood estimates:")
+print("m = {0:.3f}".format(m_ml))
+print("b = {0:.3f}".format(b_ml))
+print("f = {0:.3f}".format(np.exp(log_f_ml)))
+
+# now Bayesian
+
+def log_prior(theta):
+    m, b, log_f = theta
+    if -5.0 < m < 0.5 and 0.0 < b < 10.0 and -10.0 < log_f < 1.0:
+        return 0.0
+    return -np.inf
+
+def log_probability(theta, x, y, yerr):
+    lp = log_prior(theta)
+    if not np.isfinite(lp):
+        return -np.inf
+    return lp + log_likelihood(theta, x, y, yerr)
+
+
+# sampling posterior
+import emcee
+
+pos = soln.x + 1e-4 * np.random.randn(32, 3)
+nwalkers, ndim = pos.shape
+sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(x, y, yerr))
+sampler.run_mcmc(pos, 5000, progress=True);
+flat_samples = sampler.get_chain(discard=100, thin=15, flat=True)
+inds = np.random.randint(len(flat_samples), size=100)
+
+# plotting
+x0 = np.linspace(0, 10, 500)
+legend = 0
+
+for ind in inds:
+    sample = flat_samples[ind]
+    if legend == 0 :
+        plt.plot(x0, np.dot(np.vander(x0, 2), sample[:2]), "C1", alpha=0.1, label="Bayesian")
+        legend = 1
+    else :
+        plt.plot(x0, np.dot(np.vander(x0, 2), sample[:2]), "C1", alpha=0.1)
+
+plt.errorbar(x, y, yerr=yerr, fmt=".k", capsize=0)
+x0 = np.linspace(0, 10, 500)
+plt.plot(x0, m_true * x0 + b_true, "k", alpha=0.3, lw=3, label="truth")
+plt.plot(x0, np.dot(np.vander(x0, 2), w), "--k", label="LS")
+plt.plot(x0, np.dot(np.vander(x0, 2), [m_ml, b_ml]), ":k", label="ML")
+plt.legend(fontsize=14)
+plt.xlim(0, 10)
+plt.xlabel("x")
+plt.ylabel("y");
+
+plt.show()
+
