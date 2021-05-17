e2a87cf31f14bc88449a37c04a00b1204bd4ec35
MacielUEl <antonio.msilva@ifac.edu.br>
2021-05-17 18:51:45 -0500

    P(E) - Parte1
    Equação da Oscilações de Sabores- Parte1

--- Model/SurvivalProbability_ME.py
+++ Model/SurvivalProbability_ME.py
@@ -12,4 +12,16 @@ class SurvivalProbability_ME:
     def Pee( self, E):
         return 1.
 
+import numpy as np
 
+theta13 = 1
+theta12 = 1
+delta31 = 1
+delta32 = 1
+delta21 = 1
+
+p = 1 - (((np.sin(2*theta13))**2)*(((np.cos(theta12))**2)\
+*((np.sin(delta31))**2)+ ((np.sin(theta12))**2)*((np.sin(delta32))**2)))-\
+((np.cos(theta13))**4)*((np.sin(2*theta12))**2)*((np.sin(delta21))**2)
+
+print(p(5))
