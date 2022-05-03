9a48683deb4fdf0ce4e6ed3e6b52128d5a164c72
JulioChamorroSilva <101735878+JulioChamorroSilva@users.noreply.github.com>
2022-05-02 21:12:37 -0300

    Update SurvivalProbability_ME.py
    fixed the sqrt problem, however the resulting graphic is still a single line

--- Model/SurvivalProbability_ME.py
+++ Model/SurvivalProbability_ME.py
@@ -17,8 +17,8 @@ class SurvivalProbability_ME:
         # equations for the effect of matter added
         # all relevant variables are isolated so they can be calculated and input on the final equations
         N_e      = self._Rho/(2*UC.M_p)
-        A        = -1.*( 2*np.sqrt(2) * UC.Gf * N_e * E * UC.Hbar**3 ) #multiplying by Hbar^3 is necessary to return the variable to natural units 
-        DelM2_32 = self._DelM2_31 - self._DelM2_21
+        A        = -1.*( 2*np.sqrt(2) * UC.Gf * N_e * E * UC.Hbar**3 / UC.C) #multiplying by Hbar^3 is necessary to return the variable to natural units 
+        DelM2_32 = self._DelM2_31 - self._DelM2_21 
         DelM2_ee = np.cos(  self._Theta_12  )**2 * self._DelM2_31 + np.sin(  self._Theta_12  )**2 * DelM2_32
         DelM2_ee_ME = DelM2_ee * np.sqrt(  np.cos(  2 * self._Theta_13  )**2 + np.sin(  2 * self._Theta_13  ) )
         A_ME = (A + DelM2_ee - DelM2_ee_ME)/2 
@@ -27,12 +27,10 @@ class SurvivalProbability_ME:
         Theta_12_ME = 1 / (2 * np.cos((self._DelM2_21 * np.cos(self._Theta_13) - A_ME) / DelM2_21_ME))
         DelM2_31_ME = DelM2_ee_ME + (np.sin( Theta_12_ME )**2) * DelM2_21_ME 
         DelM2_32_ME = DelM2_31_ME - DelM2_21_ME
-        Delta21_ME = np.sqrt(DelM2_21_ME) * L * UC.Hbar / (4 * E * UC.C**3 * 1e15 ) # Delta has dimentional unit of eV^2/c * Km * GeV^-1, so to to convert GeV and eV back to natural units we have to multiply by Hbar / 10^15 * c^3
-        Delta32_ME = np.sqrt(DelM2_32_ME) * L * UC.Hbar / (4 * E * UC.C**3 * 1e15 ) # This function has show an error message for the sqrt function Delta has dimentional unit of eV^2/c * Km * GeV^-1, so to to convert GeV and eV back to natural units we have to multiply by Hbar / 10^15 * c^3
-        Delta31_ME = np.sqrt(DelM2_31_ME) * L * UC.Hbar / (4 * E * UC.C**3 * 1e15 ) # Delta has dimentional unit of eV^2/c * Km * GeV^-1, so to to convert GeV and eV back to natural units we have to multiply by Hbar / 10^15 * c^3
+        Delta21_ME = (DelM2_21_ME * L * UC.C**3 ) / (4 * E * UC.Hbar )
+        Delta32_ME = (DelM2_32_ME * L * UC.C**3 ) / (4 * E * UC.Hbar ) 
+        Delta31_ME = (DelM2_31_ME * L * UC.C**3 ) / (4 * E * UC.Hbar ) 
         term1   = np.sin( 2*Theta_13_ME )**2 * np.cos(   Theta_12_ME )**2 * np.sin( Delta31_ME )**2 
         term2   = np.sin( 2*Theta_13_ME )**2 * np.sin(   Theta_12_ME )**2 * np.sin( Delta32_ME )**2
         term3   = np.cos(   Theta_13_ME )**4 * np.sin( 2*Theta_12_ME )**2 * np.sin( Delta21_ME )**2
-        return  1 - term1 - term2 - term3
-
-
+        return  1. - term1 - term2 - term3
