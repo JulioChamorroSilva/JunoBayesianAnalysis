56bb48fef5534a2abda6e6cb98bb9faef04c5e7c
JulioChamorroSilva <101735878+JulioChamorroSilva@users.noreply.github.com>
2022-06-07 16:05:09 -0300

    Plotting of test enhanced

--- Tests/ModelTests/OscillationProbability_test_1.py
+++ Tests/ModelTests/OscillationProbability_test_1.py
@@ -17,7 +17,7 @@ prob_IH  = SurvivalProbability_ME.SurvivalProbability_ME( DelM2_21, -1.*DelM2_31
 
 L    = 52.*UC.km
 E_nu = 1.*UC.MeV 
-print("E=",E_nu/UC.MeV,"MeV,L=",L/UC.km,"km,matter effect", prob.Pee(  E_nu, L )) # E = 2 
+print("E=",E_nu/UC.MeV,"MeV,L=",L/UC.km,"km, matter effect", prob.Pee(  E_nu, L )) # E = 2 
 print("E=",E_nu/UC.MeV,"MeV,L=",L/UC.km,"km, no matter effect", prob_0.Pee(E_nu, L))
 print("E=",E_nu/UC.MeV,"MeV,L=",L/UC.km,"km, matter effect - IH", prob_IH.Pee(E_nu, L))
 
@@ -25,13 +25,23 @@ x_e      = np.linspace(1, 10, 10001)*UC.MeV
 y_pee    = [ prob.Pee( i, L ) for i in x_e ]
 y_pee_0  = [ prob_0.Pee( i, L ) for i in x_e ]
 y_pee_IH = [ prob_IH.Pee( i, L ) for i in x_e ]
-plt.plot( x_e/UC.MeV, y_pee      )
-plt.plot( x_e/UC.MeV, y_pee_IH   )
+plt.plot( x_e/UC.MeV, y_pee, label='Normal ordering', linewidth='0.5'      )
+plt.plot( x_e/UC.MeV, y_pee_IH, label='Inverted ordering', linewidth='0.5'   )
+plt.xlabel("Energy (MeV)")
+plt.ylabel("Probability")
+plt.legend()
+plt.title("Electron Neutrino Survival Probability")
+plt.grid(linestyle='--')
 plt.show()
 plt.clf()
 
 ratio_ME = [ (y_pee[i] - y_pee_0[i])/y_pee[i] for i in range(len(y_pee)) ]
 ratio_IH = [ (y_pee_IH[i] - y_pee[i])/y_pee[i] for i in range(len(y_pee)) ]
-plt.plot( x_e/UC.MeV, ratio_ME )
-plt.plot( x_e/UC.MeV, ratio_IH )
+plt.plot( x_e/UC.MeV, ratio_ME, label='P $_{normal-vacuum}$', linewidth='0.5', zorder=10 )
+plt.plot( x_e/UC.MeV, ratio_IH, label='P $_{inverted-normal}$', linewidth='0.5', zorder=0  )
+plt.xlabel("Energy (MeV)")
+plt.ylabel("Ratio of probability")
+plt.title("Ratio of probability over Normal ordering")
+plt.legend()
+plt.grid(linestyle='--')
 plt.show()
